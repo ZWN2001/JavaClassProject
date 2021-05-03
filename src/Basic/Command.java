529644574fd52306549d9e438d3f5b10a75a19c9
KirisameAi <kanatarhei@qq.com>
2021-05-03 17:39:28 +0800

    Add files via upload

--- src/Basic/Command.java
+++ src/Basic/Command.java
@@ -3,8 +3,9 @@ package Basic;
 public interface Command {
     String CLOSER_SERVER = "Close Server";
     String S_LOGIN = "Student Login";
+    String S_REFRESH_AVATAR = "Student Refresh Avatar";
     String S_REGISTER = "Student Register";
     String S_SET_AVATAR = "Student Set Avatar";
+    String S_SET_NAME = "Student Set Name";
     String S_SET_PASSWORD = "Student Set Password";
-
 }
